446d68e9b90b495d6249f1a9ab0c93fb49d00a96
tmk <tmkapoor@gmail.com>
2013-05-02 01:36:52 +0530

    styling fixed, need to fix dimensions

--- session/index.php
+++ session/index.php
@@ -9,6 +9,7 @@ if(!isset($_SESSION["nick"]) || !isset($_SESSION["sno"])){
 else{
 $_SESSION["sid"] = $_SESSION["sno"];
 unset($_SESSION["sno"]);
+$sessionURL = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 ?>
 
 <!DOCTYPE html>
@@ -28,92 +29,56 @@ unset($_SESSION["sno"]);
 
 </head>
 <body>
-	<div id="content" >
+	<div id="content">
 		<div class="jumbotron">
-			<!-- Heading banner table str -->
-			<!-- Why is this a table? We should not use tables here, this is not tabular data-->
 			<table class="table_25_75">
 				<tr>
 					<td>
 						<img src="../res/logo_32.png" />
 					</td>
 					<td>
-						<!-- This should not be done inline, should compute whatever needs to be computed in the
-						php block and just print it out here, keep logic away from the view -->
-						<h4 id="sessionURL">Session URL: <?php echo "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]; ?></h4>
+						<h4 id="sessionURL">Session URL: <?php echo $sessionURL; ?></h4>
 					</td>
 				</tr>
 			</table>
+			<!-- Overlay div -->
 			<div id="searchStuff" style="display: none;"></div>
-	<!-- Rewriting HTML for layout using divs -->
-	<div class="row">
-	  <div class="span2">Playlist+Else</div>
-	  <div class="span6" id="video">
-		<div id="bubble">
-			<div id="ytplayer">...</div>
-			<div id="controls" class="margin_1em">
-				<span unselectable="on" id="vol_down" class="vol_controls">&nbsp&nbsp-&nbsp&nbsp</span>
-				<span unselectable="on" id="vol_value">100%</span>
-				<span unselectable="on" id="vol_up" class="vol_controls">&nbsp&nbsp+&nbsp&nbsp</span>
-				<span unselectable="on">&nbsp&nbsp&nbsp&nbsp</span>
-				<span unselectable="on" id="vol_mute" class="vol_controls">&nbspmute&nbsp</span>
-		</div>
-	</div>
-	<div id="message">Initalizing...</div>
-	<div id="builder">
-			<input type="text" name="URLAdd" id="URLAdd" placeholder="Enter YouTube URL here..." disabled="disabled" /><br><br>
-			<button id="playlistBuilder" class="btn" onclick='addThings();' disabled="disabled">Initalizing...</button>
-			<button id="searchButton" class="btn" onclick='searchThings();' disabled="disabled">Initalizing...</button>
-	</div>
-	<div id="videoDetails" class="margin_1em">&nbsp</div>
-	  </div>
-	  <div class="span4" id="chat-box">
+			<!-- Rewriting HTML for layout using divs -->
 			<div class="row">
-				<div class="span4" id="messageBoard">
-					<form id="chatForm" name="chatForm" method="post" action="" onsubmit="return false;">
-					<div class="span4" id="board"><!-- Chat logs loaded here --></div>
-					</div>
-					</form>
-					<div class="span4">
-						<input type="text" id="chatBox" placeholder="Write message here...." value="Connecting to chat server..." disabled="disabled" />
-					</div>
-	  			</div>
-	</div>
-
-			<!-- Heading content table str -->
-			<table>
-				<tr>
-					<!-- Youtube stuffs -->
-					<!--<td style="text-align: center;">
-						<div id="bubble">
-							<div id="ytplayer">...</div>
-							<div id="controls" class="margin_1em">
-								<span unselectable="on" id="vol_up" class="vol_controls">&nbsp&nbsp+&nbsp&nbsp</span>
-								<span unselectable="on">vol: </span>
-								<span unselectable="on" id="vol_value">100%</span>
-								<span unselectable="on" id="vol_down" class="vol_controls">&nbsp&nbsp-&nbsp&nbsp</span>
-								<span unselectable="on">&nbsp&nbsp&nbsp&nbsp</span>
-								<span unselectable="on" id="vol_mute" class="vol_controls">&nbspmute&nbsp</span>
-							</div>
+			  	<div class="span2">Playlist+Else</div>
+			  	<div class="span6" id="video">
+					<div id="bubble">
+						<div id="ytplayer">...</div>
+						<div id="controls" class="margin_1em">
+							<span unselectable="on" id="vol_down" class="vol_controls">&nbsp&nbsp-&nbsp&nbsp</span>
+							<span unselectable="on" id="vol_value">100%</span>
+							<span unselectable="on" id="vol_up" class="vol_controls">&nbsp&nbsp+&nbsp&nbsp</span>
+							<span unselectable="on">&nbsp&nbsp&nbsp&nbsp</span>
+							<span unselectable="on" id="vol_mute" class="vol_controls">&nbspmute&nbsp</span>
 						</div>
-						<div id="message">Initalizing...</div>
-						<div id="builder">
-								<input type="text" name="URLAdd" id="URLAdd" placeholder="Enter YouTube URL here..." disabled="disabled" /><br><br>
-								<button id="playlistBuilder" class="btn" onclick='addThings();' disabled="disabled">Initalizing...</button>
-								<button id="searchButton" class="btn" onclick='searchThings();' disabled="disabled">Initalizing...</button>
-						</div>
-						<div id="videoDetails" class="margin_1em">&nbsp</div>
-					</td>-->
-					<!-- Chat stuffs -->
-					<!--<td>
-						<div id="messageBoard">
+					</div>
+					<div id="message">Initalizing...</div>
+					<div id="builder">
+							<input type="text" name="URLAdd" id="URLAdd" placeholder="Enter YouTube URL here..." disabled="disabled" /><br><br>
+							<button id="playlistBuilder" class="btn" onclick='addThings();' disabled="disabled">Initalizing...</button>
+							<button id="searchButton" class="btn" onclick='searchThings();' disabled="disabled">Initalizing...</button>
+					</div>
+					<div id="videoDetails" class="margin_1em">&nbsp</div>
+			  	</div>
+			  	<div class="span4" id="chat-box">
+					<div class="row">
+						<div class="span4" id="messageBoard">
 							<form id="chatForm" name="chatForm" method="post" action="" onsubmit="return false;">
-								<div id="board"><!-- Chat logs loaded here --><!--</div>
-								<input type="text" id="chatBox" cols=2 placeholder="Write message here...." value="Connecting to chat server..." disabled="disabled" />
+								<div class="span4" id="board"><!-- Chat logs loaded here --></div>
 							</form>
 						</div>
-				</td>-->
-		</div> 
+							<div class="span4">
+								<input type="text" id="chatBox" placeholder="Write message here...." value="Connecting to chat server..." disabled="disabled" />
+							</div>
+			  		</div>
+				</div>
+			</div> 
+		</div>
 	</div>
 
 	<!-- Le javascript
