05e9d454dd8d0a06c2ec79a99b2c1cc42bbfe54b
Kartikeya Dubey <kartikeyadubey@gmail.com>
2013-07-01 23:33:13 -0700

    Added google analytics

--- session/index.php
+++ session/index.php
@@ -163,6 +163,16 @@ $sessionURL = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 		clip.setText('<?php echo $sessionURL; ?>');
 		clip.glue('copy');
 	</script>
+			<script>
+		    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		    ga('create', 'UA-42162448-1', 'sugaryasphalt.com');
+		    ga('send', 'pageview');
+
+	</script>
 
 </body>
 </html>
